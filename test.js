342cdefcfb89994790836fc4a1a11e0ffcdd95a5
tot <zzttot4@gmail.com>
2016-08-16 22:32:15 -0700

    start tracking test.js

---
+++ test.js
@@ -0,0 +1,16 @@
+var myJSONObject = {"bindings": [
+        {"ircEvent": "PRIVMSG", "method": "newURI", "regex": "^http://.*"},
+        {"ircEvent": "PRIVMSG", "method": "deleteURI", "regex": "^delete.*"},
+        {"ircEvent": "PRIVMSG", "method": "randomURI", "regex": "^random.*"}
+    ]
+};
+var myData = JSON.parse(myJSONObject, function (key, value) {
+    var type;
+    if (value && typeof value === 'object') {
+        type = value.type;
+        if (typeof type === 'string' && typeof window[type] === 'function') {
+            return new (window[type])(value);
+        }
+    }
+    return value;
+});
